1ac95c73176f26e35718b201a7bc94423f2dcbab
PRAJAPATI ANKIT KANAIYALAL <100261566+21BCA113@users.noreply.github.com>
2022-03-12 00:41:58 -0500

    Solution 1
    Made a Change in Format of print

--- Solution 1.py
+++ Solution 1.py
@@ -1,5 +1,4 @@
 #Write a python script to enter two No. & print the sum of it..
-a=int(input("Enter value for a : "))
-b=int(input("Enter value for b : "))
-c=a+b
-print("Sum of {} and {} = {}".format(a,b,c))
+x=int(input("Enter value for a : "))
+y=int(input("Enter value for b : "))
+print("Sum of {b} and {a} = {c}".format(a=x,b=y,c=x+y))
