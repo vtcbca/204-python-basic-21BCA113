deaef83e5ae937f66a8021aa7d1692d6ed3a8856
PRAJAPATI ANKIT KANAIYALAL <100261566+21BCA113@users.noreply.github.com>
2022-03-12 00:32:42 -0500

    Solution 5
    Seperated Solution File

--- Solution 5.py
+++ Solution 5.py
@@ -1,4 +1,4 @@
-#WAS to enter any number and print from which number from 0 to 9 it is divisible.
+#WAS to enter any number and print from which number from 0 to 9 it is divisible. 
 n1=1
 n2=10
 n=int(input("Enter the number to be divided by : "))
