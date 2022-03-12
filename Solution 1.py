e336ddfb2b60ac81a8b036acdcc2088cf51332b3
PRAJAPATI ANKIT KANAIYALAL <100261566+21BCA113@users.noreply.github.com>
2022-03-12 00:29:47 -0500

    Solution 1
    Seperated FIle From All

--- Solution 1.py
+++ Solution 1.py
@@ -1,4 +1,4 @@
-#Write a python script to enter two No. & print the sum of it.
+#Write a python script to enter two No. & print the sum of it..
 a=int(input("Enter value for a : "))
 b=int(input("Enter value for b : "))
 c=a+b
